a2daf4d93833bbc76fba162d00bd1fc00f3aee11
Thiago Cruz <thagocruz@gmail.com>
2021-10-11 16:32:50 -0300

    Create PlatformRepository.cs

---
+++ PlatformService/Data/PlatformRepository.cs
@@ -0,0 +1,28 @@
+﻿using PlatformService.Models;
+using System.Collections.Generic;
+
+namespace PlatformService.Data
+{
+    public class PlatformRepository : IPlatformRepository
+    {
+        public void CreatePlatform(Platform platform)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Platform> GetAllPlatforms()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Platform GetPlatformById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool SaveChanges()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
