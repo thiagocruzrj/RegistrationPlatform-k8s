27a6c4536ff0a0de9946e7c7b071d3b3a855a430
Thiago Cruz <thagocruz@gmail.com>
2021-10-11 16:38:59 -0300

    Update PlatformRepository.cs

--- PlatformService/Data/PlatformRepository.cs
+++ PlatformService/Data/PlatformRepository.cs
@@ -1,28 +1,31 @@
 ﻿using PlatformService.Models;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace PlatformService.Data
 {
     public class PlatformRepository : IPlatformRepository
     {
+        private readonly AppDbContext _context;
+
+        public PlatformRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
         public void CreatePlatform(Platform platform)
         {
-            throw new System.NotImplementedException();
+            if (platform == null)
+                throw new ArgumentNullException(nameof(platform));
+
+            _context.Platforms.Add(platform);
         }
 
-        public IEnumerable<Platform> GetAllPlatforms()
-        {
-            throw new System.NotImplementedException();
-        }
+        public IEnumerable<Platform> GetAllPlatforms() => _context.Platforms.ToList();
 
-        public Platform GetPlatformById(int id)
-        {
-            throw new System.NotImplementedException();
-        }
+        public Platform GetPlatformById(int id) => _context.Platforms.FirstOrDefault(p => p.Id == id);
 
-        public bool SaveChanges()
-        {
-            throw new System.NotImplementedException();
-        }
+        public bool SaveChanges() => _context.SaveChanges() >= 0;
     }
 }
