67f9969220c5e22bd4151ed6e1ab70aa1b738611
Thiago Cruz <thagocruz@gmail.com>
2021-10-12 20:51:06 -0300

    Create PlatformsController.cs

---
+++ PlatformService/Controllers/PlatformsController.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PlatformService.Data;
+
+namespace PlatformService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        private readonly IPlatformRepository _repository;
+        private readonly IMapper _mapper;
+
+        public PlatformsController(IPlatformRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+    }
+}
