8abca54d9c6cf47d9f373b550546ea743511b4fc
Thiago Cruz <thagocruz@gmail.com>
2021-10-12 20:05:35 -0300

    Update PrepDb.cs

--- PlatformService/Data/PrepDb.cs
+++ PlatformService/Data/PrepDb.cs
@@ -1,8 +1,8 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using PlatformService.Models;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace PlatformService.Data
 {
@@ -10,7 +10,44 @@ namespace PlatformService.Data
     {
         public static void PrepPopulation(IApplicationBuilder app)
         {
+            using (var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>());
+            }
+        }
 
+        private static void SeedData(AppDbContext context)
+        {
+            if (!context.Platforms.Any())
+            {
+                Console.WriteLine("--> Seeding data...");
+                context.Platforms.AddRange(
+                    new Platform()
+                    {
+                        Name = "DotNet",
+                        Publisher = "Microsoft",
+                        Cost = "Free"
+                    },
+                    new Platform()
+                    {
+                        Name = "Sql Server",
+                        Publisher = "Microsoft",
+                        Cost = "Free"
+                    },
+                    new Platform()
+                    {
+                        Name = "Kubernetes",
+                        Publisher = "Microsoft",
+                        Cost = "Free"
+                    }
+                );
+
+                context.SaveChanges();
+            } 
+            else 
+            {
+                Console.WriteLine("--> We already have data");
+            }
         }
     }
 }
